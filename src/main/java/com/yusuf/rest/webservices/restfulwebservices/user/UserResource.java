4a10fd47a5f3f52b236bfd0c914f22511bb78c2a
Yusuf Alp <yusufalp@TAPL-MBP-SF.local>
2021-09-21 15:06:04 -0700

    create a RestController for Users

---
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,29 @@
+package com.yusuf.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDaoService service;
+	
+	// GET /users
+	// retrieveAllUsers - retrieve all users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	// GET /users/{id}
+	// retrieveUser(int id) - retrieve a single user
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+}
