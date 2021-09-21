da856cd3084c3fb7eb510e013250564af3cf2d1a
Yusuf Alp <yusufalp@TAPL-MBP-SF.local>
2021-09-21 14:21:51 -0700

    insert a RequestMapping with a custom path

--- src/main/java/com/yusuf/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.yusuf.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,11 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("This will return a JSON");
+	}
+	
+	//Creating a bean with a custom path
+	@GetMapping(path="/hello-world-bean/path-variable/{id}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String id) {
+		return new HelloWorldBean(String.format("Hello, %s. Welcome to your path", id));
 	}
 }
