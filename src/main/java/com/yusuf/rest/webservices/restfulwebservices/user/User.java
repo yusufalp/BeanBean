737ba94dd61ce85077fea02e5d02cf485600dc74
Yusuf Alp <yusuf.alp@parivedasolutions.com>
2021-09-22 16:09:39 -0700

    add validation requirements for name and date field

--- src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/User.java
@@ -2,9 +2,16 @@ package com.yusuf.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	private Integer id;
+	
+	@Size(min=2)
 	private String name;
+	
+	@Past
 	private Date birthDate;
 	
 	public User(Integer id, String name, Date birthDate) {
