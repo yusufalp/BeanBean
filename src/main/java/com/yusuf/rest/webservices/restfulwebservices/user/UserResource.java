a797f0eaa712e9edf75034618420872b1038e299
Yusuf Alp <yusuf.alp@parivedasolutions.com>
2021-09-22 10:12:19 -0700

    add a POST request function to insert a user

--- src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +27,12 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+	
+	// POST /users
+	// insertUser - insert a user to the list
+	@PostMapping("/users")
+	public void insertUser(@RequestBody User user) {
+		User insertedUser = service.save(user);
 	}
 }
