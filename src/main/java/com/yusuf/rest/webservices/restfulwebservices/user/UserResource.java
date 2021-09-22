eaf71b3f6d7034521262ff512ef044b709a11fb7
Yusuf Alp <yusuf.alp@parivedasolutions.com>
2021-09-22 13:01:59 -0700

    throw a new error if the user searched does not exist

--- src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.yusuf.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,13 +29,29 @@ public class UserResource {
 	// retrieveUser(int id) - retrieve a single user
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		// Throw a runtime error exception if the user does not exist
+		if(user == null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		return user;
 	}
 	
 	// POST /users
 	// insertUser - insert a user to the list
 	@PostMapping("/users")
-	public void insertUser(@RequestBody User user) {
+	public ResponseEntity<Object> insertUser(@RequestBody User user) {
 		User insertedUser = service.save(user);
+		
+		// provide the location of the newly created user in the form of URI
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(insertedUser.getId())
+			.toUri();
+		
+		// Send the created status code
+		return ResponseEntity.created(location).build();
 	}
 }
