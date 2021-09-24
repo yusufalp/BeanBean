9a5b9f5a95db625aec4e0badffc0af4ad4dc0dd1
Yusuf Alp <yusuf.alp@parivedasolutions.com>
2021-09-24 09:14:03 -0700

    include a message for the name input about its size requirement

--- src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/User.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Size;
 public class User {
 	private Integer id;
 	
-	@Size(min=2)
+	@Size(min=2, message="Name should have at least 2 characters")
 	private String name;
 	
 	@Past
