c643b0756fe9328998711f55a8b14cd9aef4c74e
Yusuf Alp <yusuf.alp@parivedasolutions.com>
2021-10-05 18:34:16 -0700

    change ids of the manually entered values so that the system can start adding from 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values(1, sysdate(),'Abyss');
-insert into user values(2, sysdate(),'Jack');
-insert into user values(3, sysdate(),'Jill');
+insert into user values(1001, sysdate(),'Abyss');
+insert into user values(1002, sysdate(),'Jack');
+insert into user values(1003, sysdate(),'Jill');
