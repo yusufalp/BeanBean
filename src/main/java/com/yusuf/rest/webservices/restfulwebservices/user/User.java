9b10a7f50629e7b4226c17cadc36b6b6cc3f4835
Yusuf Alp <yusuf.alp@parivedasolutions.com>
2021-10-05 20:10:37 -0700

    implement a relationship between Post Entity and User Entity

--- src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/yusuf/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,12 @@
 package com.yusuf.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -20,6 +22,9 @@ public class User {
 	
 	@Past
 	private Date birthDate;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 	
 	public User() {
 		
@@ -48,6 +53,12 @@ public class User {
 	}
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
